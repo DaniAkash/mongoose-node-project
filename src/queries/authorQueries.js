8b48c83a63dddf4729e2e30a04ede937eaf182a9
Dani Akash <DaniAkash@users.noreply.github.com>
2020-03-28 10:23:11 +0530

    querying author details

--- src/queries/authorQueries.js
+++ src/queries/authorQueries.js
@@ -3,31 +3,62 @@ require("../config/mongodb");
 const Author = require("../models/author");
 const Book = require("../models/book");
 
-const newAuthor = new Author({
-  first_name: "Albert",
-  last_name: "Einstein",
-  date_of_birth: new Date(1879, 2, 14),
-  field_of_study: "non-fiction",
-  date_of_death: new Date(1995, 3, 18)
-});
+// const newAuthor = new Author({
+//   first_name: "Albert",
+//   last_name: "Einstein",
+//   date_of_birth: new Date(1879, 2, 14),
+//   field_of_study: "non-fiction",
+//   date_of_death: new Date(1995, 3, 18)
+// });
 
-// 5e7ed38f2ea8f8033c488d41,
+// const newBook = new Book({
+//   title: "The world as I see it",
+//   author: "5e7ed38f2ea8f8033c488d41",
+//   summary: "Albert Einstein believes in humanity, in a peaceful world of mutual helpfulness, and in the high mission of science. This book is intended as a plea for this belief at a time which compels every one of us to overhaul his mental attitude and his ideas.",
+//   isbn: "978-0806527901"
+// });
 
-const newBook = new Book({
-  title: "The world as I see it",
-  author: "5e7ed38f2ea8f8033c488d41",
-  summary: "Albert Einstein believes in humanity, in a peaceful world of mutual helpfulness, and in the high mission of science. This book is intended as a plea for this belief at a time which compels every one of us to overhaul his mental attitude and his ideas.",
-  isbn: "978-0806527901"
-});
-
-newBook.save()
-  .then(response => {
-    console.log(response);
-  })
-  .catch(console.error)
+// newBook.save()
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(console.error)
 
 // newAuthor.save()
 //   .then(response => {
 //     console.log(response);
 //   })
 //   .catch(console.error);
+
+/**
+ * Finding all documents
+ */
+// Author.find()
+// .then(console.log)
+// .catch(console.error)
+
+/**
+ * Finding a specific document
+ */
+// Author.findById("5e7ed383f829d2031c948f6d")
+// .then(console.log)
+// .catch(console.error)
+
+/**
+ * Updating the author name
+ */
+// Author.findByIdAndUpdate("5e7ed383f829d2031c948f6d", {
+//   $set: {
+//     first_name: "Stephen",
+//     last_name: "Hawking"
+//   }
+// })
+// .then(console.log)
+// .catch(console.error)
+
+/**
+ * Finding by first name
+ */
+Author.find({first_name: "Stephen"})
+.then(console.log)
+.catch(console.error)
